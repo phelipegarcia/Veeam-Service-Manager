e967e0910e20e810b00313e5b2cb1576fb7e9407
Phelipe Garcia <phelipe_garciaa@hotmail.com>
2023-02-02 16:34:09 -0300

    Update stop_veeam.ps1
    fixing the code

--- stop_veeam.ps1
+++ stop_veeam.ps1
@@ -1,1 +1,2 @@
-Get-Service Veeam* | Stop-Service
+Get-process | where {($_.Name -like "Veeam*")} | stop-process -Force
+Get-Service | where {($_.Name -like "Veeam*")-and ($_.Status -eq "Running")} | Stop-service -Force
